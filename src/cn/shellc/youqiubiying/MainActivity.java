a86b6337e8391829a606ac8848a8187674959f3a
bainuicf <bainui@msn.com>
2015-11-19 14:27:01 +0800

    201511191426

--- src/cn/shellc/youqiubiying/MainActivity.java
+++ src/cn/shellc/youqiubiying/MainActivity.java
@@ -22,40 +22,34 @@ public class MainActivity extends Activity {
 	private void initView() {
 		// TODO Auto-generated method stub
 		token1=new Token(MainActivity.this,
-				token1,
+				"token1",
 				(ImageView)findViewById(R.id.iv_token1), 
 				(TextView)findViewById(R.id.tv_token1), 
 				(Button)findViewById(R.id.bt_token1));	
 		
 		token2=new Token(MainActivity.this,
-				token2,
+				"token2",
 				(ImageView)findViewById(R.id.iv_token2), 
 				(TextView)findViewById(R.id.tv_token2), 
 				(Button)findViewById(R.id.bt_token2));	
 		
 		token3=new Token(MainActivity.this,
-				token3,
+				"token3",
 				(ImageView)findViewById(R.id.iv_token3), 
 				(TextView)findViewById(R.id.tv_token3), 
 				(Button)findViewById(R.id.bt_token3));	
 		
 		token4=new Token(MainActivity.this,
-				token4,
+				"token4",
 				(ImageView)findViewById(R.id.iv_token4), 
 				(TextView)findViewById(R.id.tv_token4), 
-				(Button)findViewById(R.id.bt_token4));
+				(Button)findViewById(R.id.bt_token4));	
 		
-		token1.setOnClickListner(this,token1);
-		token2.setOnClickListner(this,token2);
-		token3.setOnClickListner(this,token3);
-		token4.setOnClickListner(this,token4);
 		
-		token1.changeNumber(this, token1);
-		token2.changeNumber(this, token2);
-		token3.changeNumber(this, token3);
-		token4.changeNumber(this, token4);
-		
-		//token1.sendSMS(token1);
+		token1.changeNumber(this);
+		token2.changeNumber(this);
+		token3.changeNumber(this);
+		token4.changeNumber(this);
 		
 		
 		
--- src/cn/shellc/youqiubiying/Token.java
+++ src/cn/shellc/youqiubiying/Token.java
@@ -6,6 +6,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.jar.Attributes.Name;
+
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -16,15 +20,16 @@ import android.telephony.gsm.SmsManager;
 
 @SuppressWarnings("deprecation")
 public class Token {
+	private String tokenname;
 	private ImageView tokenImage;
 	private TextView tokenText;
 	private Button tokenButton;
 
-	public Token(Context context, Token token, ImageView iv, TextView tv, Button btn) {
+	public Token(final Activity pActivity, String name, ImageView iv, TextView tv, Button btn) {
 		// TODO Auto-generated constructor stub
 
 		// 初始化兑换券数量
-		SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+		SharedPreferences sharedPreferences = pActivity.getSharedPreferences("token", Context.MODE_PRIVATE);
 		Editor editor = sharedPreferences.edit();
 		boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
 		if (isFirstRun) {
@@ -36,52 +41,53 @@ public class Token {
 		}
 		editor.commit();
 
+		setTokenname(name);
 		setTokenImage(iv);
 		setTokenText(tv);
 		setTokenButton(btn);
+		
+		tokenButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				new AlertDialog.Builder(pActivity)
+				.setTitle("确认使用")
+				.setMessage("确定使用此券吗？")
+				.setPositiveButton("莫要拦我", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						useToken(pActivity);
+					}
+				})
+				.setNegativeButton("不用也罢", null)
+				.show();
+
+			};
+		});
 
 	}
-
-	// 点击“使用”按钮
-	public void setOnClickListner(final Context context, final Token token) {
-//		tokenButton.setOnClickListener(new OnClickListener() {
-//
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				new AlertDialog.Builder(context).setTitle("确认使用").setMessage("确定使用此券吗？").setNegativeButton("不用也罢", null)
-//						.setPositiveButton("莫要拦我", new DialogInterface.OnClickListener() {
-//					public void onClick(DialogInterface dialog, int which) {
-//						useToken(context, token);
-//					}
-//				}).show();
-//
-//			};
-//		});
-
-	}
+	
 
 	// 使用兑换券
-	public void useToken(Context context, Token token) {
+	public void useToken(Context context) {
 		// TODO Auto-generated method stub
 		SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
 		Editor editor = sharedPreferences.edit();
-		int number = sharedPreferences.getInt(token.toString(), 0);
+		int number = sharedPreferences.getInt(getTokenname(), 0);
 		if (number == 0) {
 			Toast.makeText(context, "女王大人的兑换券用完，快去卖萌求宠爱吧！", Toast.LENGTH_SHORT).show();
 		} else {
 			number--;
-			editor.putInt(token.toString(), number);
+			editor.putInt(getTokenname(), number);
 			editor.commit();
-			changeNumber(context, token);
-			sendSMS(token);
+			changeNumber(context);
+			sendSMS();
 		}
 
 	}
 
-	public void sendSMS(Token token) {
+	public void sendSMS() {
 		// TODO Auto-generated method stub
 		String tokenname = null;
-		switch (token.toString()) {
+		switch (getTokenname()) {
 		case "token1":
 			tokenname = "叠衣券";
 			break;
@@ -99,18 +105,26 @@ public class Token {
 		}
 
 		SmsManager sms = SmsManager.getDefault();
-		sms.sendTextMessage("13919841220", "13893658856", "女王大人有令，赐你" + tokenname + "1张，速速领旨！\n钦此！", null, null);
+		sms.sendTextMessage("13919841220", null, "女王大人有令，赐你" + tokenname + "1张，速速领旨！\n钦此！", null, null);
 
 	}
 
-	public void changeNumber(Context context, final Token token) {
-//		SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
-//		final int number = sharedPreferences.getInt(token.toString(), 0);
+	public void changeNumber(Context context) {
+		SharedPreferences sharedPreferences = context.getSharedPreferences("token", Context.MODE_PRIVATE);
+		int number = sharedPreferences.getInt(getTokenname(), 0);
 //		tokenText.post(new Runnable() {
 //			public void run() {
-//				token.tokenText.setText(new Integer(number).toString());
+//				//tokenText.setText("10");
 //			}
 //		});
+	}
+
+	public void setTokenname(String tokenname) {
+		this.tokenname = tokenname;
+	}
+
+	public String getTokenname() {
+		return tokenname;
 	}
 
 	public void setTokenImage(ImageView tokenImage) {
