9fe8770cd5a94dfc6a3971d494f1fc4f26e3908a
bainuicf <bainui@msn.com>
2015-11-27 17:48:35 +0800

    201511271748  重新构架

--- src/cn/shellc/youqiubiying/MainActivity.java
+++ src/cn/shellc/youqiubiying/MainActivity.java
@@ -2,15 +2,20 @@ package cn.shellc.youqiubiying;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.telephony.gsm.SmsManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 
 	//@Override
@@ -18,46 +23,150 @@ public class MainActivity extends Activity {
 	private Token token1,token2,token3,token4;
 	
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		initView();	
+		super.onCreate(savedInstanceState);		
 		setContentView(R.layout.activity_main);	
 		
+		initView();
+		initNum();
 	}
 
+	
 	private void initView() {
 		// TODO Auto-generated method stub
-		token1=new Token(MainActivity.this,
+		
+		//初始化对话券
+		token1=new Token(
 				"token1",
 				(ImageView)findViewById(R.id.iv_token1), 
 				(TextView)findViewById(R.id.tv_token1), 
 				(Button)findViewById(R.id.bt_token1));	
 		
-		token2=new Token(MainActivity.this,
+		token2=new Token(
 				"token2",
 				(ImageView)findViewById(R.id.iv_token2), 
 				(TextView)findViewById(R.id.tv_token2), 
 				(Button)findViewById(R.id.bt_token2));	
 		
-		token3=new Token(MainActivity.this,
+		token3=new Token(
 				"token3",
 				(ImageView)findViewById(R.id.iv_token3), 
 				(TextView)findViewById(R.id.tv_token3), 
 				(Button)findViewById(R.id.bt_token3));	
 		
-		token4=new Token(MainActivity.this,
+		token4=new Token(
 				"token4",
 				(ImageView)findViewById(R.id.iv_token4), 
 				(TextView)findViewById(R.id.tv_token4), 
-				(Button)findViewById(R.id.bt_token4));	
-		Handler handler=new Handler();		
-		token1.changeNumber(MainActivity.this,handler);
-		token2.changeNumber(MainActivity.this,handler);
-		token3.changeNumber(MainActivity.this,handler);
-		token4.changeNumber(MainActivity.this,handler);
+				(Button)findViewById(R.id.bt_token4));
+		
+		//设置“使用”按钮监听事件
+//		token1.getTokenButton().setOnClickListener(OnTokenButtonClickListener(token1));
+//		token2.getTokenButton().setOnClickListener(OnTokenButtonClickListener(token2));
+//		token3.getTokenButton().setOnClickListener(OnTokenButtonClickListener(token3));
+//		token4.getTokenButton().setOnClickListener(OnTokenButtonClickListener(token4));
+        
+		
 		
 		
 	}
 	
+
+
+//	private OnClickListener OnTokenButtonClickListener(final Token token) {
+//		// TODO Auto-generated method stub
+	
+		 
+//		new AlertDialog.Builder(MainActivity.this)
+//		.setTitle("确认使用")
+//		.setMessage("女王大人确定使用此券吗？")
+//		.setPositiveButton("莫要拦我", new DialogInterface.OnClickListener() {
+//			public void onClick(DialogInterface dialog, int which) {
+//				useToken(token);
+//			}
+//		})
+//		.setNegativeButton("不用也罢", null)
+//		.show();
+//		return null;
+//	}
+
+
+	private void initNum() {
+		// TODO Auto-generated method stub
+		// 初始化兑换券数量
+				SharedPreferences sharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
+				Editor editor = sharedPreferences.edit();
+				boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
+				if (isFirstRun) {
+					editor.putInt("token1", 10);
+					editor.putInt("token2", 10);
+					editor.putInt("token3", 10);
+					editor.putInt("token4", 10);
+					editor.putBoolean("isFirstRun", false);
+					editor.commit();
+				}				
+				
+				changeNum(token1);
+				changeNum(token2);
+				changeNum(token3);
+				changeNum(token4);
+	}
+
+
+	private void changeNum(final Token token) {
+		// TODO Auto-generated method stub
+		SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("token", Context.MODE_PRIVATE);
+		final int number = sharedPreferences.getInt(token.getTokenname(), 0);
+		new Handler().post(new Runnable() {
+			public void run() {
+				token.getTokenText().setText(Integer.toString(number));
+			}
+		});
+	}
+	
+	// 使用兑换券
+	public void useToken(Token token) {
+		// TODO Auto-generated method stub
+		SharedPreferences sharedPreferences = getSharedPreferences("token", Context.MODE_PRIVATE);
+		Editor editor = sharedPreferences.edit();
+		int number = sharedPreferences.getInt(token.getTokenname(), 0);
+		if (number == 0) {
+			Toast.makeText(MainActivity.this, "女王大人的兑换券用完，快去卖萌求宠爱吧！", Toast.LENGTH_SHORT).show();
+		} else {
+			number--;
+			editor.putInt(token.getTokenname(), number);
+			editor.commit();
+			changeNum(token);
+			sendSMS(token);
+		}
+
+	}
+	
+	
+	//发送指令
+	public void sendSMS(Token token) {
+		// TODO Auto-generated method stub
+		String tokenname = null;
+		switch (token.getTokenname()) {
+		case "token1":
+			tokenname = "叠衣券";
+			break;
+		case "Token2":
+			tokenname = "刷鞋券";
+			break;
+		case "Token3":
+			tokenname = "按摩券";
+			break;
+		case "Token4":
+			tokenname = "任意券";
+			break;
+		default:
+			break;
+		}
+
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage("+8613919841220", null, "女王大人有令，赐你" + tokenname + "1张，速速领旨！\n钦此！", null, null);
+
+	}
 	
 	
 	
--- src/cn/shellc/youqiubiying/Token.java
+++ src/cn/shellc/youqiubiying/Token.java
@@ -1,126 +1,24 @@
 package cn.shellc.youqiubiying;
 
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.jar.Attributes.Name;
-
-import android.R.integer;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.os.Handler;
-import android.telephony.gsm.SmsManager;
-import android.util.Log;
 
-@SuppressWarnings("deprecation")
-public class Token extends Activity{
+public class Token extends Activity {
 	private String tokenname;
 	private ImageView tokenImage;
 	private TextView tokenText;
 	private Button tokenButton;
 
-	public Token(final Activity paramActivity, String name, ImageView iv, TextView tv, Button btn) {
+	public Token(String name, ImageView iv, TextView tv, Button btn) {
 		// TODO Auto-generated constructor stub
-
-		// 初始化兑换券数量
-		SharedPreferences sharedPreferences = paramActivity.getSharedPreferences("token", Context.MODE_PRIVATE);
-		Editor editor = sharedPreferences.edit();
-		boolean isFirstRun = sharedPreferences.getBoolean("isFirstRun", true);
-		if (isFirstRun) {
-			editor.putInt("token1", 10);
-			editor.putInt("token2", 10);
-			editor.putInt("token3", 10);
-			editor.putInt("token4", 10);
-			editor.putBoolean("isFirstRun", false);
-		}
-		editor.commit();
 
 		setTokenname(name);
 		setTokenImage(iv);
 		setTokenText(tv);
-		setTokenButton(btn);		
-		
-//		tokenButton.setOnClickListener(new OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				new AlertDialog.Builder(paramActivity)
-//				.setTitle("确认使用")
-//				.setMessage("确定使用此券吗？")
-//				.setPositiveButton("莫要拦我", new DialogInterface.OnClickListener() {
-//					public void onClick(DialogInterface dialog, int which) {
-//						//useToken(pActivity);
-//					}
-//				})
-//				.setNegativeButton("不用也罢", null)
-//				.show();
-//			}
-//		});
-	}
-	
-	
-	
+		setTokenButton(btn);
 
-	// 使用兑换券
-//	public void useToken(Activity pActivity) {
-//		// TODO Auto-generated method stub
-//		SharedPreferences sharedPreferences = pActivity.getSharedPreferences("token", Context.MODE_PRIVATE);
-//		Editor editor = sharedPreferences.edit();
-//		int number = sharedPreferences.getInt(getTokenname(), 0);
-//		if (number == 0) {
-//			Toast.makeText(pActivity, "女王大人的兑换券用完，快去卖萌求宠爱吧！", Toast.LENGTH_SHORT).show();
-//		} else {
-//			number--;
-//			editor.putInt(getTokenname(), number);
-//			editor.commit();
-//			changeNumber(pActivity);
-//			sendSMS();
-//		}
-
-//	}
-
-	public void sendSMS() {
-		// TODO Auto-generated method stub
-		String tokenname = null;
-		switch (getTokenname()) {
-		case "token1":
-			tokenname = "叠衣券";
-			break;
-		case "Token2":
-			tokenname = "刷鞋券";
-			break;
-		case "Token3":
-			tokenname = "按摩券";
-			break;
-		case "Token4":
-			tokenname = "任意券";
-			break;
-		default:
-			break;
-		}
-
-		SmsManager sms = SmsManager.getDefault();
-		sms.sendTextMessage("13919841220", null, "女王大人有令，赐你" + tokenname + "1张，速速领旨！\n钦此！", null, null);
-
-	}
-
-	public void changeNumber(final Activity pActivity,Handler handler) {
-		SharedPreferences sharedPreferences = pActivity.getSharedPreferences("token", Context.MODE_PRIVATE);
-		final int number = sharedPreferences.getInt(getTokenname(), 0);
-		handler.post(new Runnable() {
-			public void run() {
-//				tokenText.setText(Integer.toString(number));
-			}
-		});
 	}
 
 	public void setTokenname(String tokenname) {
